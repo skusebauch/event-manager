e98b4905d554f032530b6441dd73576a207905fd
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-25 18:01:28 +0200

    implemented category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,45 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [ :show, :edit, :update, :destroy ]
   def index
+    @categories = Category.all
+  end
+  def show
+    # find_category
+  end
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # find_category
+  end
+  def update
+    # find_category
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+  end
+  def destroy
+    # find_category
+    @category.destroy
+    redirect_to category_path
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
