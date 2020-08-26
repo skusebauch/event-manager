762b1dbe69b04ff77b00557b9c1886f54ebe3af5
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-26 17:01:04 +0200

    added proper error message at event-date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,13 +4,15 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
+  validate :start_date_after_date_today
 
   private
 
   def end_date_after_start_date
-    return if end_date.blank? || start_date.blank?
-    if end_date < start_date
-      errors.add(:end_date, "muss nach dem Start Datum liegen")
-    end
+    errors.add(:end_date, "Das Start-Datum muss vor dem End-Datum liegen") if end_date < start_date
+  end
+
+  def start_date_after_date_today
+    errors.add(:start_date, "Das Start-Datum kann nicht in der Vergangenheit liegen") if start_date < Date.today
   end
 end
